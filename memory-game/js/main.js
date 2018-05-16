999ea35ca2a014763144344e8bd0f74619b2f24a
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-05-16 18:44:48 -0500

    added functions to js main

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,20 +1,28 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
-//check if length of arrawy is 2 
-if(cardsInPlay.length === 2) {
-    //check if the two cards in the array are the same
-    if(cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-    } else {
-        alert("Sorry, try again.");
-    }
-}
-//
+var checkForMatch = function() {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}; 
+
+var flipCard = function(card) {
+	console.log("User flipped " + cards[card]);
+	cardsInPlay.push(cards[card]);
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
