147b5ffe0958312da859f6d5b82176afc1984e4f
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-05-15 19:10:19 -0500

    updated javascript

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+//check if length of arrawy is 2 
+if(cardsInPlay.length === 2) {
+    //check if the two cards in the array are the same
+    if(cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
+//
