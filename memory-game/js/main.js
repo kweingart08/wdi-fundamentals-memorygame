abbec03476ac9aa8505b1e5a2decb6b90afe7475
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-05-17 18:30:59 -0500

    fixed js alert timing

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -23,30 +23,17 @@ var cards = [
 
 var cardsInPlay = [];
 
-
-var checkForMatch = function() {
-	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-}; 
-
-setTimeout(checkForMatch, 1000);
-
 var flipCard = function() {
 	var cardId = this.getAttribute("data-id");
-
-	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-
 	this.setAttribute("src", cards[cardId].cardImage)
 
-	if(cardsInPlay.length === 2) {
-		checkForMatch();
-	}
+if(cardsInPlay.length === 2) {
+	checkForMatch();
+}
 };
 
 var createBoard = function() {
@@ -60,3 +47,14 @@ var createBoard = function() {
 };
 
 createBoard();
+
+var checkForMatch = function() {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		setTimeout(function(){alert("You found a match!");}, 300);
+	} else {
+		setTimeout(function(){alert("Sorry, try again.");}, 300);
+	}
+}; 
+
+
+
